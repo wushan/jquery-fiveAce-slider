68e7d0ad4ac3f12e8a3cb17007f30cb27e9e4086
wushan <chiangloveslynn@gmail.com>
2016-05-25 18:08:38 +0800

    improvements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,6 +33,7 @@
           Wrapper.prepend(controls);
       var _defaultSettings = {
           'gap' : 30,
+          'itemHeight': 200,
           'item' : '.item'
       };
 
@@ -41,10 +42,18 @@
 
       return this.each(function(){
         console.log('initial');
+        //Set Item Size
+        items.css({
+          'height': _settings.itemHeight,
+          'overflow': 'hidden'
+        });
 
         //Scale Container Size
-        currentWrapper.css('height', items.outerHeight() + (items.length-1)*_settings.gap);
-
+        currentWrapper.css('height', items.outerHeight());
+        //Bind Container Resize on Window Resize
+        $(window).resize(function(){
+          currentWrapper.css('height', items.outerHeight());
+        })
         //Bind Controller Event
         prevBtn.on('click', function(){
           slidePrev(currentWrapper, _settings);
@@ -56,7 +65,7 @@
         //Init Positioning
         items.each(function(index){
           console.log($(this));
-          $(this).css('top', _settings.gap*index);
+          $(this).css('bottom', _settings.gap*index*-1);
           $(this).css('z-index', items.length-index );
           $(this).attr('data-rel', index);
         });
@@ -73,84 +82,94 @@
     
     var tempWrapper = $('<div class=tempWrapper></div>')
     
+    tempWrapper.css({
+      'z-index': 9999,
+      'position': 'absolute',
+      'width': '100%',
+      'bottom': options.gap*-1
+    });
+
     //Wrap all items except first Element
     currentWrapper.children(':not(:first-child)').wrapAll(tempWrapper);
+    
+    //Reposition items
+    $('.tempWrapper').children(options.item).each(function(index){
+      $(this).css('bottom', options.gap*index*-1 );
+      $(this).css('z-index', $('.tempWrapper').children().length+1-index );
+      $(this).attr('data-rel', index);
+    })
 
-    $('.tempWrapper').css({
-      'z-index': 9999,
-      'position': 'relative',
-      'width': '100%',
-      'top': 0
-    });
-    //Remove first item
-      currentWrapper.children(options.item).remove();
-      $('.tempWrapper').children(options.item).each(function(index){
-        $(this).css('top', options.gap*index );
-        $(this).css('z-index', $('.tempWrapper').children().length-index );
-        $(this).attr('data-rel', index);
-      })
-
-      fakeEl.css('z-index',0);
-      fakeEl.css('top',$('.tempWrapper').children().length*options.gap+options.gap);
-      fakeEl.css('opacity',0);
+    fakeEl.css('z-index',1);
+    fakeEl.css('bottom',($('.tempWrapper').children().length-1)*options.gap*-1);
+    fakeEl.css('position', 'absolute');
     //
     $('.tempWrapper').animate({
-      top: options.gap*-1/3
+      bottom: 0
     }, 300, function(){
-      
+      //Remove first item
+      currentWrapper.children(options.item).remove();
       //Load New item from bottom
       $('.tempWrapper').append(fakeEl);
+      console.log('appe');
       // fakeEl.css('top',$('.tempWrapper').children().length*options.gap-options.gap);
       fakeEl.animate({
-        top: $('.tempWrapper').children().length*options.gap-options.gap,
-        opacity: 1
-      }, 300)
-      //Release
-      $('.tempWrapper').children(options.item).unwrap();
+        bottom: ($('.tempWrapper').children().length-1)*options.gap*-1
+      }, 300, function(){
+        //Release
+        // $('.tempWrapper').children(options.item).unwrap();
+        // console.log($(this));
+        $(this).unwrap();
+      })
     });
-  
   }
   function slidePrev(currentWrapper, options){
     console.log('go prev');
     //Fake Item
-    var fakeEl = $(currentWrapper).children(options.item + ':first-child').clone();
+    var fakeEl = $(currentWrapper).children(options.item + ':last-child').clone();
+    
     var tempWrapper = $('<div class=tempWrapper></div>')
     
+    tempWrapper.css({
+      'z-index': 9999,
+      'position': 'absolute',
+      'width': '100%',
+      'bottom': 0
+    });
+
     //Wrap all items except first Element
     currentWrapper.children(':not(:last-child)').wrapAll(tempWrapper);
 
-    $('.tempWrapper').css({
-      'z-index': 9999,
-      'position': 'relative',
-      'width': '100%',
-      'top': 0
-    });
-    //Remove last item
-      currentWrapper.children(options.item).remove();
-      $('.tempWrapper').children(options.item).each(function(index){
-        $(this).css('top', options.gap*index+options.gap );
-        $(this).css('z-index', $('.tempWrapper').children().length-index);
-        $(this).attr('data-rel', index);
-      })
-
-      fakeEl.css('z-index', 6);
-      fakeEl.css('top', options.gap*-1);
-      fakeEl.css('opacity', 0);
-
+    fakeEl.css('z-index',$('.tempWrapper').children().length+1);
+    fakeEl.css('bottom', options.gap);
+    fakeEl.css('position', 'absolute');
+    // fakeEl.css('opacity', 0);
     //
     $('.tempWrapper').animate({
-      top: options.gap
+      bottom: options.gap*-1
     }, 300, function(){
-      
+      //Remove first item
+      console.log('ddd');
+      currentWrapper.children(options.item).remove();
+      //Reposition items
+      $('.tempWrapper').children(options.item).each(function(index){
+        $(this).css('bottom', options.gap*(index+1)*-1 );
+        $(this).css('z-index', $('.tempWrapper').children().length-index );
+        $(this).attr('data-rel', index);
+      })
       //Load New item from bottom
       $('.tempWrapper').prepend(fakeEl);
+      console.log('appe');
       // fakeEl.css('top',$('.tempWrapper').children().length*options.gap-options.gap);
       fakeEl.animate({
-        top: 0,
-        opacity: 1
-      }, 300)
-      //Release
-      $('.tempWrapper').children(options.item).unwrap();
+        bottom: 0
+      });
+      $('.tempWrapper').animate({
+        bottom: 0
+      },300,function(){
+        fakeEl.unwrap();
+      });
+
+      
     });
   }
 })(jQuery);
